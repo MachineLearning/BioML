b16cbc7a88d799c6c188645285a01dae8dcbe5ab
Nikolaos Abatzis <nikolaos@abatzis.com>
2012-07-26 10:08:19 -0500

    Main program ver 2.0, using separated data for train,cv,test
    Still remaining to add the graphing of j_train, j_cv

--- biologicalResponse.m
+++ biologicalResponse.m
@@ -7,21 +7,18 @@ printf('\nBiological Response Learning\n') ;
 
 %% =========== Part 1: Training Data Loading
 
-% Load Data from a subdirectory called 'data'. 
-% Assumptions for the training data file:
-%    1. it is called 'train.mat'
-%    2. the data is normalized
-%    3. the y column vector is the ... column
 printf("\nLoading TRAINING data ... ") ;
-
 load("BioData.mat");
 
-[m, cols] = size(X) ;
-printf("done. Dimension for X is : %d X %d\n", m, cols);
-printf("Dimension for y is : %d X %d\n", size(y));
+% Split the training data into train, CV and test
+[X_train, y_train, X_cv, y_cv, X_test, y_test] = segmentDataset(X, y) ;
+
+[m, cols] = size(X_train) ;
+printf("done. Dimension for X_train is : %d X %d\n", m, cols);
+printf("Dimension for y_train is : %d X %d\n", size(y_train));
 
 % Determine whether the data is skewed
-%[ isskewed, skewnesspc ] = skewness( y, 0.1 ) ;
+%[ isskewed, skewnesspc ] = skewness( y_train, 0.1 ) ;
 isskewed = false ;
 
 aux = '' ;
@@ -37,7 +34,7 @@ pause (3) ;
 printf("Learning using Logistic Regression. ") ;
 
 % Initialize fitting parameters
-initial_theta = zeros(size(X, 2), 1);
+initial_theta = zeros(size(X_train, 2), 1);
 
 % Set regularization parameter lambda to 1
 lambda = 1;
@@ -55,24 +52,27 @@ options = optimset('GradObj', 'on', 'MaxIter', 100);
 DATAPOINTS_NEEDED = 20 ;
 
 step_for_m = ceil(m / DATAPOINTS_NEEDED) ;
-m_values = zeros(DATAPOINTS_NEEDED, 1) ;
-
 m_count = step_for_m ;
-for i = 1:DATAPOINTS_NEEDED
-   X_train = X(1:m_count, :) ;
-   y_train = y(1:m_count, :) ;
 
-   printf("\nSize of X_train is: %d X %d", size(X_train)) ;
-   printf("\nSize of y_train is: %d X %d", size(y_train)) ;
-   printf("\nSize of initial_theta is: %d X %d", size(initial_theta)) ;
+m_values = zeros(DATAPOINTS_NEEDED, 1) ;
+J_train_values = zeros(DATAPOINTS_NEEDED, 1) ;
+J_cv_values = zeros(DATAPOINTS_NEEDED, 1) ;
+
+for i = 1:DATAPOINTS_NEEDED
+   X_used = X_train(1:m_count, :) ;
+   y_used = y_train(1:m_count, :) ;
+
+   printf("\nSize of X_used is: %d X %d", size(X_used)) ;
+   printf("\nSize of y_used is: %d X %d", size(y_used)) ;
    % Optimize
    [theta, J, exit_flag] = ...
-	fminunc(@(t)(lrCostFunction(t, X_train, y_train, lambda)), initial_theta, options);
+	fminunc(@(t)(lrCostFunction(t, X_used, y_used, lambda)), initial_theta, options);
 
    printf("\nLearning completed. Lambda used: %f # of samples used(m): %d\n",lambda, m_count) ;
    
    % store the values to be plotted. NEED TO ADD J_cv
    J_train_values(i) = J ;
+   J_cv_values(i) = lrCostFunction(theta, X_cv, y_cv, lambda) ;
    m_values(i) = m_count ;
    
    % increase the number of samples used, making sure we never exceed m
@@ -88,6 +88,6 @@ printf("Test data dimension is : %d X %d\n", m_test, cols);
 
 p = predict(theta, X_test);
 
-%printf('Train Accuracy: %f\n', mean(double(p == y_test)) * 100);
+printf("Train Accuracy: %f\n", mean(double(p == y_test)) * 100);
 
 
