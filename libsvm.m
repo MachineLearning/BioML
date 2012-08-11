397e93b8f879b73a3b468aa520c0f791219bf8a6
kartik khanna <kartikaye83@gmail.com>
2012-08-10 22:51:52 -0700

    Libsvm 1st version

--- libsvm.m
+++ libsvm.m
@@ -26,13 +26,21 @@ end
 Xtrain = [Xtrain; Xcv];
 ytrain = [ytrain; ycv];
 
+%Libsvm usage
+%Transform data to conform with the tool
+
 features_sparse = sparse(Xtrain);
 libsvmwrite('Xtrain_sp', ytrain, features_sparse);
 
 [ytrain_vec, instance_matrix] = libsvmread('Xtrain_sp'); 
 
+%Initialize values of C and gamma for Cross-validation.
+%Setup to perform a coarse-grid search. Scope to add another   
+%layer for fine-grid search.
+
 C = [0.01; 0.1; 1; 5; 10; 20; 30; 50; 100; 1000];
 gamma = [2^-5; 2^-3; 2^-1; 1; 2; 4; 8; 16; 32];
+
 lenC = length(C);
 lenG = length(gamma);
 
@@ -43,6 +51,11 @@ k = 1;
 
 t0 = clock();
 
+%Loop through C/gamma combinations, and initialize other 
+%parameters to use with training.
+%RBF kernel.
+%10-fold cross validation. 
+
 for i = 1:lenC
 	for j = 1:lenG
 	kernel = 2; cost = C(i); gam = gamma(j); shrink = 0; cv = 10; 
@@ -51,20 +64,30 @@ for i = 1:lenC
 	k = k + 1;
 	end
 end
+
 time_elapsed = etime(clock(), t0);
 printf("\n elapsed time (in secs): %d", time_elapsed);
 fflush(stdout);
 
+%Matrix cross_val contains accuracy for each run.
+
 index = find(cross_val(:,3) == max(cross_val(:,3)));
 
+%C=1.00000000e+000, gamma=3.12500000e-002, Accuracy=7.72075975e+001%
+
 options2 = ['-c ' , num2str(cross_val(index,1)), ' -t ', num2str(kernel), ' -g ', num2str(cross_val(index,2)), ' -h ', num2str(shrink), ' -q'];
+
 model = svmtrain(ytrain_vec, instance_matrix, options2);
+
 [predict_label, accuracy, dec_values] = svmpredict(ytrain_vec, instance_matrix, model);
+
 err = sum(ytrain_vec + predict_label==0)
 
 sparse_Xtest = sparse(Xtest);
 libsvmwrite('Xtest_sp', ytest, sparse_Xtest);
 
 [ytest_vec, instance_matrix_test] = libsvmread('Xtest_sp');
+
 [predict_test, accuracy_test, dec_values_test] = svmpredict(ytest_vec, instance_matrix_test, model);
+
 err_test = sum(ytest_vec + predict_test==0)
